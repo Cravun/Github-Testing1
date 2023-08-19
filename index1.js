072de0abbdd3b73ea8595d1e055a757257d0c1ea
Fern Santigo <fdsantiago2020@plm.edu.ph>
2023-08-19 19:33:09 +0800

    .

--- index1.js
+++ index1.js
@@ -2,6 +2,15 @@ export default function Testing1() {
   return (
     <>
       <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
+      <h1>tanginamo alberto</h1>
     </>
   );
 }
